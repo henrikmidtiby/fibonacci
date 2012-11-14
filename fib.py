159d6c507665de54fe3b573e1107c5db65dcc76c
Henrik Skov Midtiby <hemi@kbm.sdu.dk>
2012-11-14 09:13:06 +0100

    Implementation stubs

--- fib.py
+++ fib.py
@@ -1,5 +1,14 @@
 # Program for calculating fibonacci numbers.
 
+def fibRecursive(n):
+	return 1
+
+def fibIterative(n):
+	return 1
+
+def fibDirect(n):
+	return 1
+
 # Main method for displaying fibonacci numbers.
 def main():
 	fibonaccinumbers = [1, 1, 2, 3, 5, 8, 13, 21, 34]
