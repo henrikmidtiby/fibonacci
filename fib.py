77bdbecbc2d349fbd7ba6a8c46e4460e60017923
Henrik Skov Midtiby <hemi@kbm.sdu.dk>
2012-11-14 09:11:20 +0100

    Added comment

--- fib.py
+++ fib.py
@@ -1,5 +1,6 @@
 # Program for calculating fibonacci numbers.
 
+# Main method for displaying fibonacci numbers.
 def main():
 	fibonaccinumbers = [1, 1, 2, 3, 5, 8, 13, 21, 34]
 	for fib in fibonaccinumbers:
